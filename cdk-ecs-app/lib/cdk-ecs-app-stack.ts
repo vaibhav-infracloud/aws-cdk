4af11db50a636de31d4fe8f56d75dbd3f0345308
vaibhav-infracloud <vaibhav.gupta@infracloud.io>
2023-08-10 15:35:14 +0530

    Adding support for multi-service deployment reusing a same ALB listening on different ports

--- cdk-ecs-app/lib/cdk-ecs-app-stack.ts
+++ cdk-ecs-app/lib/cdk-ecs-app-stack.ts
@@ -5,11 +5,24 @@ import * as ecs from 'aws-cdk-lib/aws-ecs';
 import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2';
 
 // Some configurable parameters
-const image_name = "public.ecr.aws/docker/library/httpd:latest"
-const container_port = 80
-const alb_listener_port = 80
-const memory = '512' // in Mb
-const cpu = '256' 
+const services = [
+  {
+  image_name : "public.ecr.aws/docker/library/httpd:latest",
+  container_port : 80,
+  alb_listener_port : 8081,
+  desired_count : 2,
+  memory : '512', // in Mb
+  cpu : '256'
+ },
+ {
+   image_name : "nginx:latest",
+   container_port : 80,
+   alb_listener_port : 8080,
+   desired_count : 2,
+   memory : '512', // in Mb
+   cpu : '256'
+ }
+];
 
 export class CdkEcsAppStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -25,48 +38,56 @@ export class CdkEcsAppStack extends Stack {
       vpc: vpc,
     });
 
-    // Create a task definition
-        // Create a task definition
-    const taskDefinition = new ecs.TaskDefinition(this, 'CDKTaskDefinition', {
-      compatibility: ecs.Compatibility.FARGATE,
-      memoryMiB: memory,
-      cpu: cpu,
-    });
-
-    // Add container(s) to the task definition
-    taskDefinition.addContainer('CDKContainer', {
-      image: ecs.ContainerImage.fromRegistry(image_name),
-      memoryLimitMiB: parseInt(memory, 10),
-      portMappings: [{ containerPort: container_port }],
-    });
-
-    // Create a service
-    const service = new ecs.FargateService(this, 'CDKService', {
-      cluster: cluster,
-      taskDefinition: taskDefinition,
-    });
-
     // Create an Application Load Balancer
     const alb = new elbv2.ApplicationLoadBalancer(this, 'CDKALB', {
       vpc: vpc,
       internetFacing: true,
     });
 
-    // Create a listener for the ALB
-    const listener = alb.addListener('CDKListener', {
-      port: alb_listener_port,
-      open: true,
+    // Create a task definition
+        // Create a task definition
+    services.forEach((service, index ) => {
+
+        const taskDefinition = new ecs.TaskDefinition(this,'CDKTaskDefinition_'+index, {
+          compatibility: ecs.Compatibility.FARGATE,
+          memoryMiB: service.memory,
+          cpu: service.cpu,
+        });
+    
+        // Add container(s) to the task definition
+        taskDefinition.addContainer('CDKContainer_'+index, {
+          image: ecs.ContainerImage.fromRegistry(service.image_name),
+          memoryLimitMiB: parseInt(service.memory, 10),
+          portMappings: [{ containerPort: service.container_port }],
+        });
+    
+        // Create a service
+        const ecsService = new ecs.FargateService(this, 'CDKService_'+index, {
+          cluster: cluster,
+          taskDefinition: taskDefinition,
+          desiredCount: service.desired_count
+        });
+        
+        // Create a listener for the ALB
+        const listener = alb.addListener('CDKListener_'+index, {
+          port: service.alb_listener_port,
+          protocol: elbv2.ApplicationProtocol.HTTP,
+          open: true,
+        });
+    
+        // Attach the ECS service as a target to the ALB listener
+        listener.addTargets('ECS', {
+          port: service.container_port,
+          targets: [ecsService],
+        });
+
+        // Output the ALB DNS name
+        
     });
 
-    // Attach the ECS service as a target to the ALB listener
-    listener.addTargets('ECS', {
-      port: container_port,
-      targets: [service],
-    });
-
-    // Output the ALB DNS name
     new CfnOutput(this, 'ALBDNS', {
       value: alb.loadBalancerDnsName,
+      description: 'ALB DNS name for service',
     });
   }
 }
